24e6d18e3b58bf056aaab0fe277ff29b667e83bf
JoannaGrab <joannagrab.jg@gmail.com>
2021-04-15 15:01:22 +0200

    make discs appear in right order

--- hanoi.js
+++ hanoi.js
@@ -3,98 +3,21 @@ var from = [],
     to = [],
     numberOfDiscs;
 
+var fromPeg,
+    sparePeg,
+    toPeg;
+
 // Take selected discs from user
 var discsHTML = document.getElementById("discs");
 
-// Assign value to numberOfDiscs and update "from" peg according to user selection
-function initialState() {
-    numberOfDiscs = discsHTML.value;
-    for (let i = 0; i < numberOfDiscs; i++) {
-        from.push(i+1);
-    }
-    drawTowers();
-}
+// Set initial state of towers
+discsHTML.onclick = () => setInitialState();
 
-// Draw initial state of towers
-discsHTML.onclick = () => initialState();
-
-// Play button
 var playButton = document.getElementById("playButton");
 playButton.onclick = () => hanoiTower();
 
-function drawTowers() {
-    let fromPeg = document.getElementById("from-peg");
-    fromPeg.innerHTML = "";
-    for (disc in from) {
-        let li = document.createElement("li");
-        li.innerHTML = from[disc];
-        fromPeg.appendChild(li);
-    }
-    from.length = 0;
 
-    let sparePeg = document.getElementById("spare-peg");
-    sparePeg.innerHTML = "";
-    for (disc in spare) {
-        let li = document.createElement("li");
-        li.innerHTML = spare[disc];
-        sparePeg.appendChild(li);
-    }
-    spare.length = 0;
-
-    let toPeg = document.getElementById("to-peg");
-    toPeg.innerHTML = "";
-    for (disc in to) {
-        let li = document.createElement("li");
-        li.innerHTML = to[disc];
-        toPeg.appendChild(li);
-    }
-    to.length = 0;
-}
-
-
-
-function makeLegalMove(discOne, discTwo, pegOne, pegTwo){
-    // if Two peg is empty
-     if (discTwo === undefined) {
-        // if One peg isn't empty
-        if (discOne !== undefined) {
-            // console.log(`can make legal move from discOne: ${discOne} to discTwo: ${discTwo}`);
-            pegTwo.push(pegOne.pop());
-        }
-    }
-    // if One peg is empty
-    else if (discOne === undefined) {
-        // if Two peg isn't empty
-        if (discTwo !== undefined) 
-        {
-            // console.log(`can make legal move from discTwo: ${discTwo} to discOne: ${discOne}`);
-            pegOne.push(pegTwo.pop());
-        }
-    }
-    // if both pegs aren't empty
-    else {
-        // console.log(`pegs aren't empty`);
-        // discOne is greater than discTwo -> can't move
-        if (discOne > discTwo) {
-            // console.log(`discOne > discTwo: ${discOne} > ${discTwo}, can't move`);
-            pegOne.push(pegTwo.pop());
-        }
-        // discOne is less than discTwo -> can move
-        else {
-            // console.log(`discOne < discTwo: ${discOne} < ${discTwo}, can move`);
-            pegTwo.push(pegOne.pop());
-        }
-    }
-}
-
-
-function isEven(number){
-    return (number % 2 == 0) ? true : false;
-}
-
-// ITERATIVE SOLUTION
 function hanoiTower() {
-
     var moves = Math.pow(2, numberOfDiscs) - 1;
 
     console.log("before")
@@ -104,43 +27,163 @@ function hanoiTower() {
 
     if (isEven(numberOfDiscs)) {
         while(moves >= 0){
-            // console.log(`discOne is on peg: ${from} (from), discTwo is on peg: ${spare} (spare)`);
             makeLegalMove(from[from.length-1], spare[spare.length-1], from, spare);
+            console.log("mid")
+            console.log(from)
+            console.log(spare)
+            console.log(to)
+            drawTowers(true);
             moves--;
             if (moves == 0) break;
 
-            // console.log(`discOne is on peg: ${from} (from), discTwo is on peg: ${to} (to)`);
             makeLegalMove(from[from.length-1], to[to.length-1], from, to);
+            console.log("mid")
+            console.log(from)
+            console.log(spare)
+            console.log(to)
+            drawTowers(true);
             moves--;
             if (moves == 0) break;
 
-            // console.log(`discOne is on peg: ${spare} (spare), discTwo is on peg: ${to} (to)`);
             makeLegalMove(spare[spare.length-1], to[to.length-1], spare, to);
+            console.log("mid")
+            console.log(from)
+            console.log(spare)
+            console.log(to)
+            drawTowers(true);
             moves--;
             if (moves == 0) break;
         }
     }
     else {
         while(moves >= 0){
-            // console.log(`discOne is on peg: ${from} (from), discTwo is on peg: ${to} (to)`);
             makeLegalMove(from[from.length-1], to[to.length-1], from, to);
+            console.log("mid")
+            console.log(from)
+            console.log(spare)
+            console.log(to)
+            drawTowers(true);
             moves--;
             if (moves == 0) break;
 
-            // console.log(`discOne is on peg: ${from} (from), discTwo is on peg: ${spare} (spare)`);
             makeLegalMove(from[from.length-1], spare[spare.length-1], from, spare);
+            console.log("mid")
+            console.log(from)
+            console.log(spare)
+            console.log(to)
+            drawTowers(true);
             moves--;
             if (moves == 0) break;
 
-            // console.log(`discOne is on peg: ${spare} (spare), discTwo is on peg: ${to} (to)`);
             makeLegalMove(spare[spare.length-1], to[to.length-1], spare, to);
+            console.log("mid")
+            console.log(from)
+            console.log(spare)
+            console.log(to)
+            drawTowers(true);
             moves--;
             if (moves == 0) break;
         }
     }
+}
 
-    console.log("after")
-    console.log(from)
-    console.log(spare)
-    console.log(to)
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// INITIALIZE PEGS
+function setInitialState() {
+    from.length = 0;
+    spare.length = 0;
+    to.length = 0;
+
+    numberOfDiscs = discsHTML.value;
+    for (let i = numberOfDiscs; i > 0; i--) {
+        from.push(+i);
+    }
+    drawTowers();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// CHECK NUMBER
+function isEven(number){
+    return (number % 2 == 0) ? true : false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// MOVE DISCS
+function makeLegalMove(discOne, discTwo, pegOne, pegTwo){
+    if (discTwo === undefined) {
+        if (discOne !== undefined) {
+            pegTwo.push(pegOne.pop());
+            drawTowers(true);
+        }
+    }
+    else if (discOne === undefined) {
+        if (discTwo !== undefined) {
+            pegOne.push(pegTwo.pop());
+            drawTowers(true);
+        }
+    }
+    else {
+        if (discOne > discTwo) {
+            pegOne.push(pegTwo.pop());
+            drawTowers();
+        }
+        else {
+            pegTwo.push(pegOne.pop());
+            drawTowers(true);
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// DRAW TOWERS
+async function drawTowers(wait) {
+    if (wait) {
+        for (let i = 0; i < 1000; i++)
+        {
+            console.log("fed")
+        }
+    }
+    drawFromPeg();
+    drawSparePeg();
+    drawToPeg();
+}
+
+function drawFromPeg() {
+    fromPeg = document.getElementById("from-peg");
+    from.reverse()
+    fromPeg.innerHTML = "";
+    for (disc in from) {
+        let li = document.createElement("li");
+        li.innerHTML = from[disc];
+        fromPeg.appendChild(li);
+        
+    }
+    from.reverse()
+}
+
+function drawSparePeg() {
+    sparePeg = document.getElementById("spare-peg");
+    spare.reverse()
+    sparePeg.innerHTML = "";
+    for (disc in spare) {
+        let li = document.createElement("li");
+        li.innerHTML = spare[disc];
+        sparePeg.appendChild(li);
+    }
+    spare.reverse()
+}
+
+function drawToPeg() {
+    toPeg = document.getElementById("to-peg");
+    to.reverse()
+    toPeg.innerHTML = "";
+    for (disc in to) {
+        let li = document.createElement("li");
+        li.innerHTML = to[disc];
+        toPeg.appendChild(li);
+    }
+    to.reverse()
 }
